nlnwa/hermetic

commit 60d388416df2c3fc89cbe338226cf2f6e27096eb

feat: minimal transfer implementation
# Motivation

Minimal implementation of the interface defined in commit
d6a9fd5c6a8d99ac36cca361fe65af1dad594f69. One major feature it is
missing is sending alerts to users upon failures.

# Implementation

`ParseKafkaEndpointsFlags` will be shared with a follow up commit that
adds the verification implementation.

One of the implementation details that is important to note is how the
Submission Information Package (SIP) is created.

The SIP contains the following fields:

* Date
* ContentCategory
* ContentType
* Identifier
* Urn
* Path

For the web archive project and for uploading harvested pages
specifically:
```
Date: time of SIP creation
ContentCategory: nettarkiv
ContentType: warc
Identifier: no-nb_<ContentCategory>_<Path.name>
Urn: URN:NBN:<Identifier>
Path: Path to directory to be uploaded
```

For example, the following SIP is valid:
```
Date: 2023-09-03T15:04:05.000
ContentCategory: nettarkiv
ContentType: warc
Identifier: no-nb_nettarkiv_kommuner_2023-20230611002729-0035-veidemann-contentwriter-568c6f8545-frvcm
Urn: URN:NBN:no-nb_nettarkiv_kommuner_2023-20230611002729-0035-veidemann-contentwriter-568c6f8545-frvcm
Path: /stage-artifacts-root/kommuner_2023-20230611002729-0035-veidemann-contentwriter-568c6f8545-frvcm
```

# Future work

The current implementation is a very minimal one. It does not alert
users upon failures through correct channels, so the current solution is
dependent on the user checking the logs manually to discover failures.

Another task that will be completed later is to make the transfer
implementation to use the `kafka` message offset to track what files it
has already created and sent SIPs for.

internal/send/send.go
@@ -1,0 +1,102 @@
+package sendImplementation
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/segmentio/kafka-go"
+)
+
+type TransferSubmissionInformationPackage struct {
+	Date            string `json:"date"`
+	ContentCategory string `json:"contentCategory"`
+	ContentType     string `json:"contentType"`
+	Identifier      string `json:"identifier"`
+	Urn             string `json:"urn"`
+	Path            string `json:"path"`
+}
+
+type sender struct {
+	writer *kafka.Writer
+}
+
+func PrepareAndSendSubmissionInformationPackage(kafkaEndpoints []string, transferTopicName string, rootPath string) error {
+	sender := sender{
+		writer: &kafka.Writer{
+			Addr:     kafka.TCP(kafkaEndpoints...),
+			Topic:    transferTopicName,
+			Balancer: &kafka.LeastBytes{},
+		},
+	}
+
+	defer sender.writer.Close()
+
+	items, err := os.ReadDir(rootPath)
+	if err != nil {
+		return fmt.Errorf("failed to read root path '%s', original error: '%w'", rootPath, err)
+	}
+	for _, path := range items {
+		if path.IsDir() {
+			directoryName := path.Name()
+			destinationPath := rootPath + directoryName
+			fmt.Printf("Processing directory %s\n", destinationPath)
+			transferSubmissionInformationPackage := createSubmissionInformationPackage(destinationPath, directoryName)
+
+			kafkaMessage, err := json.Marshal(transferSubmissionInformationPackage)
+			if err != nil {
+				return fmt.Errorf("failed to marshal json, original error: '%w'", err)
+			}
+
+			err = sender.sendMessageToKafkaTopic(kafkaMessage)
+			if err != nil {
+				return fmt.Errorf("failed to send message to kafka topic '%s', original error: '%w'", transferTopicName, err)
+			}
+		} else {
+			return fmt.Errorf("found file '%s' in root path '%s', but expected only directories", path.Name(), rootPath)
+		}
+	}
+
+	return nil
+}
+
+func createSubmissionInformationPackage(payloadPath string, payloadDirName string) TransferSubmissionInformationPackage {
+	date := time.Now().UTC().Format("2006-01-02T15:04:05.000")
+	contentCategory := "nettarkiv"
+	contentType := "warc"
+	identifier := "no-nb_" + contentCategory + "_" + payloadDirName
+	urn := "URN:NBN:" + identifier
+
+	return TransferSubmissionInformationPackage{
+		Date:            date,
+		ContentCategory: contentCategory,
+		ContentType:     contentType,
+		Identifier:      identifier,
+		Urn:             urn,
+		Path:            payloadPath,
+	}
+}
+
+func (sender *sender) sendMessageToKafkaTopic(payload []byte) error {
+	kafkaMessageUuid := uuid.New()
+	fmt.Printf("Sending message with uuid %s\n", kafkaMessageUuid)
+	kafkaMessageUuidBytes, err := kafkaMessageUuid.MarshalText()
+	if err != nil {
+		return fmt.Errorf("failed to marshal uuid, original error: '%w'", err)
+	}
+
+	err = sender.writer.WriteMessages(context.Background(),
+		kafka.Message{
+			Key:   kafkaMessageUuidBytes,
+			Value: payload,
+		},
+	)
+	if err != nil {
+		return fmt.Errorf("failed to write messages, original error: '%w'", err)
+	}
+
+	return nil
+}

internal/send/send_test.go
@@ -1,0 +1,55 @@
+package sendImplementation
+
+import (
+	"testing"
+	"time"
+)
+
+func TestCreateSubmissionInformationPackage(t *testing.T) {
+	dateFormat := "2006-01-02T15:04:05.000"
+	expectedSubmissionInformationPackage := TransferSubmissionInformationPackage{
+		Date:            time.Now().UTC().Format(dateFormat),
+		ContentCategory: "nettarkiv",
+		ContentType:     "warc",
+		Identifier:      "no-nb_nettarkiv_nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt",
+		Urn:             "URN:NBN:no-nb_nettarkiv_nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt",
+		Path:            "/path/to/nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt/nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt.warc.gz",
+	}
+
+	submissionInformationPackage := createSubmissionInformationPackage("/path/to/nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt/nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt.warc.gz", "nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt")
+
+	if expectedSubmissionInformationPackage.ContentCategory != submissionInformationPackage.ContentCategory {
+		t.Errorf("Expected %s, got %s", expectedSubmissionInformationPackage.ContentCategory, submissionInformationPackage.ContentCategory)
+	}
+
+	if expectedSubmissionInformationPackage.ContentType != submissionInformationPackage.ContentType {
+		t.Errorf("Expected %s, got %s", expectedSubmissionInformationPackage.ContentType, submissionInformationPackage.ContentType)
+	}
+
+	if expectedSubmissionInformationPackage.Identifier != submissionInformationPackage.Identifier {
+		t.Errorf("Expected %s, got %s", expectedSubmissionInformationPackage.Identifier, submissionInformationPackage.Identifier)
+	}
+
+	if expectedSubmissionInformationPackage.Urn != submissionInformationPackage.Urn {
+		t.Errorf("Expected %s, got %s", expectedSubmissionInformationPackage.Urn, submissionInformationPackage.Urn)
+	}
+
+	if expectedSubmissionInformationPackage.Path != submissionInformationPackage.Path {
+		t.Errorf("Expected %s, got %s", expectedSubmissionInformationPackage.Path, submissionInformationPackage.Path)
+	}
+
+	expectedDate, err := time.Parse(dateFormat, expectedSubmissionInformationPackage.Date)
+	if err != nil {
+		t.Errorf("Expected %s, got %s", expectedDate, expectedSubmissionInformationPackage.Date)
+	}
+	date, err := time.Parse(dateFormat, submissionInformationPackage.Date)
+	if err != nil {
+		t.Errorf("Expected %s, got %s", date, submissionInformationPackage.Date)
+	}
+
+	isBeforeCalculatedDate := date.Compare(expectedDate)
+
+	if isBeforeCalculatedDate < 0 {
+		t.Errorf("Expected %s to be before %s", date, expectedDate)
+	}
+}

cmd/send/send.go
@@ -2,8 +2,15 @@ package send
 
 import (
 	"fmt"
+	"hermetic/internal/common_flags"
+	sendImplementation "hermetic/internal/send"
 
 	"github.com/spf13/cobra"
+)
+
+const (
+	transferTopicFlagName      string = "transfer-topic"
+	stageArtifactsRootFlagName string = "stage-artifacts-root"
 )
 
 func NewCommand() *cobra.Command {
@@ -13,14 +20,12 @@ func NewCommand() *cobra.Command {
 		Args:  cobra.NoArgs,
 		RunE:  parseArgumentsAndCallSend,
 	}
-	transferTopicFlagName := "transfer-topic"
 	command.Flags().String(transferTopicFlagName, "", "name of transfer-topic")
 	if markTransferRequiredError := command.MarkFlagRequired(transferTopicFlagName); markTransferRequiredError != nil {
 		panic(fmt.Sprintf("failed to mark flag %s as required, original error: '%s'", transferTopicFlagName, markTransferRequiredError))
 	}
 
-	rootPathFlagName := "stage-artifacts-root"
-	command.Flags().String(rootPathFlagName, "", `path to the root directory with the following content structure:
+	command.Flags().String(stageArtifactsRootFlagName, "", `path to the root directory with the following content structure:
 /stage-artifacts-root
 ├── /kommuner_2023-20230611002729-0035-veidemann-contentwriter-568c6f8545-frvcm
 │   ├── /kommuner_2023-20230611002729-0035-veidemann-contentwriter-568c6f8545-frvcm.warc.gz
@@ -29,19 +34,22 @@ func NewCommand() *cobra.Command {
     ├── /kommuner_2023-20230611002730-0036-veidemann-contentwriter-568c6f8545-frvcm.warc.gz
     └── /checksum_transferred.md5
 ... etc`)
-	if markRootPathRequiredError := command.MarkFlagRequired(rootPathFlagName); markRootPathRequiredError != nil {
-		panic(fmt.Sprintf("failed to mark flag %s as required, original error: '%s'", rootPathFlagName, markRootPathRequiredError))
+	if err := command.MarkFlagRequired(stageArtifactsRootFlagName); err != nil {
+		panic(fmt.Sprintf("failed to mark flag %s as required, original error: '%s'", stageArtifactsRootFlagName, err))
 	}
 
 	return command
 }
 
 func parseArgumentsAndCallSend(cmd *cobra.Command, args []string) error {
-	fmt.Println("Parsing send arguments")
-	return send()
-}
+	transferTopicName, err := cmd.Flags().GetString(transferTopicFlagName)
+	if err != nil {
+		return fmt.Errorf("getting transfer topic name failed, original error: '%w'", err)
+	}
+	stageArtifactsRoot, err := cmd.Flags().GetString("stage-artifacts-root")
+	if err != nil {
+		return fmt.Errorf("getting stage artifacts root failed, original error: '%w'", err)
+	}
 
-func send() error {
-	fmt.Println("Running send")
-	return nil
+	return sendImplementation.PrepareAndSendSubmissionInformationPackage(common_flags.KafkaEndpoints, transferTopicName, stageArtifactsRoot)
 }

cmd/send/send_test.go
@@ -1,11 +1,0 @@
-package send
-
-import (
-	"testing"
-)
-
-func TestValidateCmdName(t *testing.T) {
-	if err := send(); err != nil {
-		panic("send failed")
-	}
-}

go.mod
@@ -2,9 +2,15 @@ module hermetic
 
 go 1.20
 
-require github.com/spf13/cobra v1.7.0
+require (
+	github.com/segmentio/kafka-go v0.4.42
+	github.com/spf13/cobra v1.7.0
+)
 
 require (
+	github.com/google/uuid v1.3.0 // indirect
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
+	github.com/klauspost/compress v1.15.9 // indirect
+	github.com/pierrec/lz4/v4 v4.1.15 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )

go.sum
@@ -1,10 +1,66 @@
 github.com/cpuguy83/go-md2man/v2 v2.0.2/go.mod h1:tgQtvFlXSQOSOSIRvRPT7W67SCa46tRHOmNcaadrF8o=
+github.com/davecgh/go-spew v1.1.0/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
+github.com/davecgh/go-spew v1.1.1 h1:vj9j/u1bqnvCEfJOwUhtlOARqs3+rkHYY13jYWTU97c=
+github.com/davecgh/go-spew v1.1.1/go.mod h1:J7Y8YcW2NihsgmVo/mv3lAwl/skON4iLHjSsI+c5H38=
+github.com/google/uuid v1.3.0 h1:t6JiXgmwXMjEs8VusXIJk2BXHsn+wx8BZdTaoZ5fu7I=
+github.com/google/uuid v1.3.0/go.mod h1:TIyPZe4MgqvfeYDBFedMoGGpEw/LqOeaOT+nhxU+yHo=
 github.com/inconshreveable/mousetrap v1.1.0 h1:wN+x4NVGpMsO7ErUn/mUI3vEoE6Jt13X2s0bqwp9tc8=
 github.com/inconshreveable/mousetrap v1.1.0/go.mod h1:vpF70FUmC8bwa3OWnCshd2FqLfsEA9PFc4w1p2J65bw=
+github.com/klauspost/compress v1.15.9 h1:wKRjX6JRtDdrE9qwa4b/Cip7ACOshUI4smpCQanqjSY=
+github.com/klauspost/compress v1.15.9/go.mod h1:PhcZ0MbTNciWF3rruxRgKxI5NkcHHrHUDtV4Yw2GlzU=
+github.com/pierrec/lz4/v4 v4.1.15 h1:MO0/ucJhngq7299dKLwIMtgTfbkoSPF6AoMYDd8Q4q0=
+github.com/pierrec/lz4/v4 v4.1.15/go.mod h1:gZWDp/Ze/IJXGXf23ltt2EXimqmTUXEy0GFuRQyBid4=
+github.com/pmezard/go-difflib v1.0.0 h1:4DBwDE0NGyQoBHbLQYPwSUPoCMWR5BEzIk/f1lZbAQM=
+github.com/pmezard/go-difflib v1.0.0/go.mod h1:iKH77koFhYxTK1pcRnkKkqfTogsbg7gZNVY4sRDYZ/4=
 github.com/russross/blackfriday/v2 v2.1.0/go.mod h1:+Rmxgy9KzJVeS9/2gXHxylqXiyQDYRxCVz55jmeOWTM=
+github.com/segmentio/kafka-go v0.4.42 h1:qffhBZCz4WcWyNuHEclHjIMLs2slp6mZO8px+5W5tfU=
+github.com/segmentio/kafka-go v0.4.42/go.mod h1:d0g15xPMqoUookug0OU75DhGZxXwCFxSLeJ4uphwJzg=
 github.com/spf13/cobra v1.7.0 h1:hyqWnYt1ZQShIddO5kBpj3vu05/++x6tJ6dg8EC572I=
 github.com/spf13/cobra v1.7.0/go.mod h1:uLxZILRyS/50WlhOIKD7W6V5bgeIt+4sICxh6uRMrb0=
 github.com/spf13/pflag v1.0.5 h1:iy+VFUOCP1a+8yFto/drg2CJ5u0yRoB7fZw3DKv/JXA=
 github.com/spf13/pflag v1.0.5/go.mod h1:McXfInJRrz4CZXVZOBLb0bTZqETkiAhM9Iw0y3An2Bg=
+github.com/stretchr/objx v0.1.0/go.mod h1:HFkY916IF+rwdDfMAkV7OtwuqBVzrE8GR6GFx+wExME=
+github.com/stretchr/objx v0.4.0/go.mod h1:YvHI0jy2hoMjB+UWwv71VJQ9isScKT/TqJzVSSt89Yw=
+github.com/stretchr/testify v1.7.1/go.mod h1:6Fq8oRcR53rry900zMqJjRRixrwX3KX962/h/Wwjteg=
+github.com/stretchr/testify v1.8.0 h1:pSgiaMZlXftHpm5L7V1+rVB+AZJydKsMxsQBIJw4PKk=
+github.com/stretchr/testify v1.8.0/go.mod h1:yNjHg4UonilssWZ8iaSj1OCr/vHnekPRkoO+kdMU+MU=
+github.com/xdg-go/pbkdf2 v1.0.0 h1:Su7DPu48wXMwC3bs7MCNG+z4FhcyEuz5dlvchbq0B0c=
+github.com/xdg-go/pbkdf2 v1.0.0/go.mod h1:jrpuAogTd400dnrH08LKmI/xc1MbPOebTwRqcT5RDeI=
+github.com/xdg-go/scram v1.1.2 h1:FHX5I5B4i4hKRVRBCFRxq1iQRej7WO3hhBuJf+UUySY=
+github.com/xdg-go/scram v1.1.2/go.mod h1:RT/sEzTbU5y00aCK8UOx6R7YryM0iF1N2MOmC3kKLN4=
+github.com/xdg-go/stringprep v1.0.4 h1:XLI/Ng3O1Atzq0oBs3TWm+5ZVgkq2aqdlvP9JtoZ6c8=
+github.com/xdg-go/stringprep v1.0.4/go.mod h1:mPGuuIYwz7CmR2bT9j4GbQqutWS1zV24gijq1dTyGkM=
+github.com/yuin/goldmark v1.4.13/go.mod h1:6yULJ656Px+3vBD8DxQVa3kxgyrAnzto9xy5taEt/CY=
+golang.org/x/crypto v0.0.0-20190308221718-c2843e01d9a2/go.mod h1:djNgcEr1/C05ACkg1iLfiJU5Ep61QUkGW8qpdssI0+w=
+golang.org/x/crypto v0.0.0-20210921155107-089bfa567519/go.mod h1:GvvjBRRGRdwPK5ydBHafDWAxML/pGHZbMvKqRZ5+Abc=
+golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4/go.mod h1:jJ57K6gSWd91VN4djpZkiMVwK6gcyfeH4XE8wZrZaV4=
+golang.org/x/net v0.0.0-20190620200207-3b0461eec859/go.mod h1:z5CRVTTTmAJ677TzLLGU+0bjPO0LkuOLi4/5GtJWs/s=
+golang.org/x/net v0.0.0-20210226172049-e18ecbb05110/go.mod h1:m0MpNAwzfU5UDzcl9v0D8zg8gWTRqZa9RBIspLL5mdg=
+golang.org/x/net v0.0.0-20220722155237-a158d28d115b/go.mod h1:XRhObCWvk6IyKnWLug+ECip1KBveYUHfp+8e9klMJ9c=
+golang.org/x/net v0.7.0 h1:rJrUqqhjsgNp7KqAIc25s9pZnjU7TUcSY7HcVZjdn1g=
+golang.org/x/net v0.7.0/go.mod h1:2Tu9+aMcznHK/AK1HMvgo6xiTLG5rD5rZLDS+rp2Bjs=
+golang.org/x/sync v0.0.0-20190423024810-112230192c58/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
+golang.org/x/sync v0.0.0-20220722155255-886fb9371eb4/go.mod h1:RxMgew5VJxzue5/jJTE5uejpjVlOe/izrB70Jof72aM=
+golang.org/x/sys v0.0.0-20190215142949-d0b11bdaac8a/go.mod h1:STP8DvDyc/dI5b8T5hshtkjS+E42TnysNCUPdjciGhY=
+golang.org/x/sys v0.0.0-20201119102817-f84b799fce68/go.mod h1:h1NjWce9XRLGQEsW7wpKNCjG9DtNlClVuFLEZdDNbEs=
+golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
+golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
+golang.org/x/sys v0.0.0-20220722155257-8c9f86f7a55f/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
+golang.org/x/sys v0.5.0/go.mod h1:oPkhp1MJrh7nUepCBck5+mAzfO9JrbApNNgaTdGDITg=
+golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1/go.mod h1:bj7SfCRtBDWHUb9snDiAeCFNEtKQo2Wmx5Cou7ajbmo=
+golang.org/x/term v0.0.0-20210927222741-03fcf44c2211/go.mod h1:jbD1KX2456YbFQfuXm/mYQcufACuNUgVhRMnK/tPxf8=
+golang.org/x/term v0.5.0/go.mod h1:jMB1sMXY+tzblOD4FWmEbocvup2/aLOaQEp7JmGp78k=
+golang.org/x/text v0.3.0/go.mod h1:NqM8EUOU14njkJ3fqMW+pc6Ldnwhi/IjpwHt7yyuwOQ=
+golang.org/x/text v0.3.3/go.mod h1:5Zoc/QRtKVWzQhOtBMvqHzDpF6irO9z98xDceosuGiQ=
+golang.org/x/text v0.3.7/go.mod h1:u+2+/6zg+i71rQMx5EYifcz6MCKuco9NR6JIITiCfzQ=
+golang.org/x/text v0.3.8/go.mod h1:E6s5w1FMmriuDzIBO73fBruAKo1PCIq6d2Q6DHfQ8WQ=
+golang.org/x/text v0.7.0 h1:4BRB4x83lYWy72KwLD/qYDuTu7q9PjSagHvijDw7cLo=
+golang.org/x/text v0.7.0/go.mod h1:mrYo+phRRbMaCq/xk9113O4dZlRixOauAjOtrjsXDZ8=
+golang.org/x/tools v0.0.0-20180917221912-90fa682c2a6e/go.mod h1:n7NCudcB/nEzxVGmLbDWY5pfWTLqBcC2KZ6jyYvM4mQ=
+golang.org/x/tools v0.0.0-20191119224855-298f0cb1881e/go.mod h1:b+2E5dAYhXwXZwtnZ6UAqBI28+e2cm9otk0dWdXHAEo=
+golang.org/x/tools v0.1.12/go.mod h1:hNGJHUnrk76NpqgfD5Aqm5Crs+Hm0VOH/i9J2+nxYbc=
+golang.org/x/xerrors v0.0.0-20190717185122-a985d3407aa7/go.mod h1:I/5z698sn9Ka8TeJc9MKroUUfqBBauWjQqLJ2OPfmY0=
 gopkg.in/check.v1 v0.0.0-20161208181325-20d25e280405/go.mod h1:Co6ibVJAznAaIkqp8huTwlJQCZ016jof/cbN4VW5Yz0=
+gopkg.in/yaml.v3 v3.0.0-20200313102051-9f266ea9e77c/go.mod h1:K4uyk7z7BCEPqu6E+C64Yfv1cQ7kz7rIZviUmN+EgEM=
+gopkg.in/yaml.v3 v3.0.1 h1:fxVm/GzAzEWqLHuvctI91KS9hhNmmWOoWu0XTYJS7CA=
 gopkg.in/yaml.v3 v3.0.1/go.mod h1:K4uyk7z7BCEPqu6E+C64Yfv1cQ7kz7rIZviUmN+EgEM=

commit 60b567a650a57039bd0e6f125f47b1392d8d3b68

fix: use `UUID` in identifier
# Motivation

Commit 60d388416df2c3fc89cbe338226cf2f6e27096eb mistakenly assumed that
`URN`s are unchangeable, but this is not the case. In certain
situations, there might be a reupload of the same `URN`, for example
when something was uploaded with the wrong name.

The true unchangeable reference to the object is the `identifier`, which
with the current implementation is not going to be unique on a reupload.

Ideally the `identifier` should not be provided by `hermetic`, but be
returned by `Digital Preservation System` (DPS), but unfortunately this
is not the case.

# Workaround

To avoid this problem we simply add a `UUID` to the end of the
`identifier` and unlink the `URN` from the `identifier`. The UUID
version is version 4.

Theoretically, there is a chance that the `UUID` is not unique, but the
likelihood is, according to
[Wikipedia](https://en.wikipedia.org/wiki/Universally_unique_identifier#Collisions),
this:

```
For example, the number of random version-4 UUIDs which need to be
generated in order to have a 50% probability of at least one collision
is 2.71 quintillion
```

internal/send/send.go
@@ -12,6 +12,7 @@ import (
 	kafkaHelpers "hermetic/internal/kafka"
 
 	"github.com/allegro/bigcache/v3"
+	"github.com/google/uuid"
 	"github.com/segmentio/kafka-go"
 )
 
@@ -207,8 +208,9 @@ func createSubmissionInformationPackage(payloadPath string, payloadDirName strin
 	date := time.Now().UTC().Format("2006-01-02T15:04:05.000")
 	contentCategory := "nettarkiv"
 	contentType := "warc"
-	identifier := "no-nb_" + contentCategory + "_" + payloadDirName
-	urn := "URN:NBN:" + identifier
+	commonPart := "no-nb_" + contentCategory + "_" + payloadDirName
+	identifier := commonPart + "_" + uuid.New().String()
+	urn := "URN:NBN:" + commonPart
 
 	return TransferSubmissionInformationPackage{
 		Date:            date,

internal/send/send_test.go
@@ -1,6 +1,7 @@
 package sendImplementation
 
 import (
+	"regexp"
 	"testing"
 	"time"
 )
@@ -11,7 +12,7 @@ func TestCreateSubmissionInformationPackage(t *testing.T) {
 		Date:            time.Now().UTC().Format(dateFormat),
 		ContentCategory: "nettarkiv",
 		ContentType:     "warc",
-		Identifier:      "no-nb_nettarkiv_nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt",
+		Identifier:      "no-nb_nettarkiv_nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt_[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}", //https://regex101.com/r/0XbiJ6/1
 		Urn:             "URN:NBN:no-nb_nettarkiv_nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt",
 		Path:            "/path/to/nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt/nettaviser_SCREENSHOT_2023-20230718002403-0216-veidemann-contentwriter-5bb4677d67-qwtmt.warc.gz",
 	}
@@ -26,8 +27,10 @@ func TestCreateSubmissionInformationPackage(t *testing.T) {
 		t.Errorf("Expected %s, got %s", expectedSubmissionInformationPackage.ContentType, submissionInformationPackage.ContentType)
 	}
 
-	if expectedSubmissionInformationPackage.Identifier != submissionInformationPackage.Identifier {
-		t.Errorf("Expected %s, got %s", expectedSubmissionInformationPackage.Identifier, submissionInformationPackage.Identifier)
+	var compiledIdentifier = regexp.MustCompile(expectedSubmissionInformationPackage.Identifier)
+
+	if !compiledIdentifier.MatchString(submissionInformationPackage.Identifier) {
+		t.Errorf("Expected %s to match %s", submissionInformationPackage.Identifier, compiledIdentifier)
 	}
 
 	if expectedSubmissionInformationPackage.Urn != submissionInformationPackage.Urn {