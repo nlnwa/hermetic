ce2a3bd92f33495d8c022502488c48588cbc434d
Andreas Børsheim <andreas.borsheim@nb.no>
2024-08-30 12:47:06 +0200

    feat: Add optional flag for sending confirm message to API
    When hermetic recevies a confirm message, we want to relay the response from DPS to update a database with the files location in DPS.

--- cmd/verify/confirm/confirm.go
+++ cmd/verify/confirm/confirm.go
@@ -12,6 +12,11 @@ import (
 	"os/signal"
 )
 
+const (
+	confirmTopicFlagName           string = "confirm-topic"
+	confirmMessageReceiverFlagName string = "confirm-message-receiver"
+)
+
 func NewCommand() *cobra.Command {
 	command := &cobra.Command{
 		Use:   "confirm",
@@ -19,18 +24,25 @@ func NewCommand() *cobra.Command {
 		Args:  cobra.NoArgs,
 		RunE:  parseArgumentsAndReadConfirmTopic,
 	}
-	confirmTopicFlagName := "confirm-topic"
 	command.Flags().String(confirmTopicFlagName, "", "name of confirm-topic")
 	if err := command.MarkFlagRequired(confirmTopicFlagName); err != nil {
 		panic(fmt.Sprintf("failed to mark flag %s as required, original error: '%s'", confirmTopicFlagName, err))
 	}
+
+	command.Flags().String(confirmMessageReceiverFlagName, "", "optional URL for confirm message receiver")
+
 	return command
 }
 
 func parseArgumentsAndReadConfirmTopic(cmd *cobra.Command, args []string) error {
-	confirmTopicName, err := cmd.Flags().GetString("confirm-topic")
+	confirmTopicName, err := cmd.Flags().GetString(confirmTopicFlagName)
 	if err != nil {
 		return fmt.Errorf("failed to get confirm-topic flag, cause: `%w`", err)
+	}
+
+	confirmMessageReceiverUrl, err := cmd.Flags().GetString(confirmMessageReceiverFlagName)
+	if err != nil {
+		return fmt.Errorf("failed to get confirm-message-receiver flag, cause: `%w`", err)
 	}
 
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
@@ -42,7 +54,11 @@ func parseArgumentsAndReadConfirmTopic(cmd *cobra.Command, args []string) error 
 		GroupID: "nettarkivet-hermetic-verify-confirm",
 	})
 
-	err = confirmImplementation.ReadConfirmTopic(ctx, reader)
+	if confirmMessageReceiverUrl != "" {
+		err = confirmImplementation.ReadConfirmTopic(ctx, reader, confirmMessageReceiverUrl)
+	} else {
+		err = confirmImplementation.ReadConfirmTopic(ctx, reader)
+	}
 	if err != nil {
 		err = fmt.Errorf("verification error, cause: `%w`", err)
 		fmt.Printf("Sending error message to Teams\n")
--- internal/verify/confirm/confirm.go
+++ internal/verify/confirm/confirm.go
@@ -1,18 +1,64 @@
 package confirmImplmentation
 
 import (
+	"bytes"
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/segmentio/kafka-go"
 	"hermetic/internal/dps"
+	"net/http"
+	"net/url"
+	"time"
 )
 
-func ReadConfirmTopic(ctx context.Context, reader *kafka.Reader) error {
+func ReadConfirmTopic(ctx context.Context, reader *kafka.Reader, receiverUrl ...string) error {
 	for {
 		response, err := dps.ReadMessages(ctx, reader)
 		if err != nil {
 			return fmt.Errorf("failed to read message from confirm-topic: `%w`", err)
 		}
-		fmt.Printf("SIP successfully preserved: %+v\n", response)
+		if len(receiverUrl) > 0 {
+			err = SendConfirmMessage(receiverUrl[0], response.DPSResponse)
+			if err != nil {
+				return fmt.Errorf("failed to send confirm message: `%w`", err)
+			}
+		} else {
+			fmt.Printf("Received message: %v\n", response.DPSResponse)
+		}
 	}
+}
+
+func SendConfirmMessage(baseUrl string, response dps.DigitalPreservationSystemResponse) error {
+	url, err := url.JoinPath(baseUrl, response.Identifier)
+	if err != nil {
+		return fmt.Errorf("failed to join URL path: `%w`", err)
+	}
+
+	jsonData, err := json.Marshal(response)
+	if err != nil {
+		return fmt.Errorf("failed to marshal DPS response to JSON: `%w`", err)
+	}
+
+	req, err := http.NewRequest("POST", url, bytes.NewBuffer(jsonData))
+	if err != nil {
+		return fmt.Errorf("failed to create HTTP request: `%w`", err)
+	}
+	req.Header.Set("Content-Type", "application/json")
+
+	client := &http.Client{
+		Timeout: 10 * time.Second,
+	}
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return fmt.Errorf("failed to send HTTP request: `%w`", err)
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+	}
+	fmt.Printf("Successfully sent confirm message to %s\n", url)
+	return nil
 }
